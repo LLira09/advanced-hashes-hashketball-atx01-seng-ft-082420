83a03432ce2ab60ca363ca85f82c55b75888a16e
Jake <llira09@gmail.com>
2020-08-01 21:21:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,7 +165,7 @@ def player_numbers(t_name)
   game_hash.each do |team, team_stats|
     if team_stats[:team_name] == t_name
       team_stats[:player].map do |key, points|
-        points[:points]
+        points[:number]
       end
     end
   end
